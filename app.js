c3848f85922655741ee39fb801db6946af9f7b17
ajr-zimmer <ajr.zimmer@gmail.com>
2017-03-10 01:42:42 -0700

    Got a ragged implementation of server generated usernames

--- app.js
+++ app.js
@@ -9,15 +9,26 @@ app.use(express.static(__dirname + '/public'));
 var port = '3000';
 app.set('port', port);
 
+var users = {};
+var namesUsed = [];
+var counter = 0;
+
 io.on('connection', function(client) {
-    console.log('a user connected');
-    client.on('disconnect', function() {
-        console.log('user disconnected');
-    });
+    console.log('a user connected with id:' + client.id);
+    users[client.id] = "User"+counter;
+    counter +=1;
+    io.emit('update users', users);
+
     // When client sends a message, broadcast to everyone
     client.on('send message', function(msg) {
         console.log('message: ' + msg);
         io.emit('display message', msg);
+    });
+
+    client.on('disconnect', function() {
+        console.log('user disconnected with id:' + client.id);
+        delete users[client.id];
+        io.emit('update users', users);
     });
 });
 
--- public/js/client.js
+++ public/js/client.js
@@ -3,15 +3,23 @@ $(function() {
   var client = io();
   $('form').submit(function(){
     var chatLog = $('#messagesWrap');
-    chatLog.animate({scrollTop: chatLog.prop('scrollHeight')}, 1000);
+    chatLog.animate({scrollTop: chatLog.prop('scrollHeight')}, 250);
     if(isNonEmptyString($('#m').val())){
       client.emit('send message', $('#m').val());
       $('#m').val('');
     }
     return false;
   });
+
   client.on('display message', function(msg){
     $('#messages').append($('<li>').text(msg));
+  });
+
+  client.on('update users', function(users){
+    $('#usernames').empty();
+    $.each(users, function(clientId, username){
+      $('#usernames').append($('<li>').text(username));
+    });
   });
 });
 
